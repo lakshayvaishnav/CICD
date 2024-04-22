609956144bf8376ead867bef4b0a7c8cf66352fa
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-04-22 15:26:44 +0530

    new branch added

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,6 +4,10 @@ import { authOptions } from "./lib/auth";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
+  console.log(session);
+  console.log("hello testing");
+  console.log("hello testing");
+  console.log("hello testing");
   if (session?.user) {
     redirect("/dashboard");
   } else {
