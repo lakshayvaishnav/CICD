9a03b0fc08283d01ed6f94bebbe712886c1e298e
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-04-22 15:25:45 +0530

    error test

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,23 +4,6 @@ import { authOptions } from "./lib/auth";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  console.log(session);
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
-  console.log("hello testing");
   if (session?.user) {
     redirect("/dashboard");
   } else {
